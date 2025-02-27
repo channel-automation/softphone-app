33d42585372254ed163a26b7f6b0238cb6e961e7
Denver <vergamer99@gmail.com>
2025-02-27 18:37:37 +0800

    add Embedded Qonvo AI chat

---
+++ Softphone.Frontend/Controllers/LiveChatController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Softphone.Frontend.Controllers
+{
+    [Authorize]
+    public class LiveChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
